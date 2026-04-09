0f71bb18ebfa0982709636b1f34bf1d396f90999
Deppisch Detailing <deppischdetailing@gmail.com>
2026-04-08 22:47:36 -0400

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+});
